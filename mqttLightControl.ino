eeafeedba9ab02b06a00bd72be2149afe8249eba
Marco Koch <marco@5th.ch>
2012-10-22 21:50:47 +0200

    basic on/off/toggle features & got rid off subscription check and basic delay loop

--- mqttLightControl.ino
+++ mqttLightControl.ino
@@ -3,15 +3,30 @@
 #include <SPI.h>
 
 int led = 9;
-bool check = true;
 
 // Update these with values suitable for your network.
 byte mac[]    = {  0x90, 0xA2, 0xDA, 0x60, 0x68, 0x64 };
 byte server[] = { 172, 16, 0, 70 };
 byte ip[]     = { 172, 16, 0, 44 };
 
-void callback(char* topic, byte* payload, unsigned int length) {
-  toggle(led);
+void callback(char* topic, byte* payload, unsigned int length)
+{
+  
+  if(length == 2 && payload[0] == 'o' && payload[1] == 'n')
+  {
+    on(led);
+  }
+  
+  if(length == 3 && payload[0] == 'o' && payload[1] == 'f' && payload[2] == 'f')
+  {
+    off(led);
+  }
+
+  if(length == 0 || length == 6 && payload[0] == 't' && payload[1] == 'o' && payload[2] == 'g' && payload[3] == 'g' && payload[4] == 'l' && payload[5] == 'e')
+  {
+    toggle(led);
+  }
+
 }
 
 PubSubClient client(server, 1883, callback);
@@ -27,33 +42,43 @@ void loop()
   
   if(client.connected())
   {
-      client.loop();
-      
-      if (check)
-      {
-        client.subscribe("byteli/light/1");
-        check = false;
-      }
-      
+      client.loop();   
   }
   else
   {
     client.connect("arduinoLightControl");
-    delay(2000);
+    delay(5000);
+    client.subscribe("byteli/light/1");
   }
-
-  delay(500);
  
 }
 
-void toggle(int pin)
+void on(int pin)
+{
+  if(digitalRead(pin) == LOW)
+  {
+    digitalWrite(pin, HIGH);
+  }
+}
+
+void off(int pin)
 {
   if(digitalRead(pin) == HIGH)
   {
     digitalWrite(pin, LOW);
   }
-  else
+}
+
+void toggle(int pin)
+{
+  if(digitalRead(pin) == LOW)
   {
     digitalWrite(pin, HIGH);
   }
+  else
+  {
+    digitalWrite(pin, LOW);
+  }
 }
+
+
